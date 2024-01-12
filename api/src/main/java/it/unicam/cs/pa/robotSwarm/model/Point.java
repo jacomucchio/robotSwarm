98af51416c71d363a158a76248d12af03a4b0c6c
Jacopo Mucchietto <jacopo.mucchietto@studenti.unicam.it>
2024-01-12 18:41:02 +0100

    Implemented Point and PointTest

--- api/src/main/java/it/unicam/cs/pa/robotSwarm/model/Point.java
+++ api/src/main/java/it/unicam/cs/pa/robotSwarm/model/Point.java
@@ -1,4 +1,56 @@
 package it.unicam.cs.pa.robotSwarm.model;
 
+import java.util.Objects;
+
 public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    // Metodo per calcolare la distanza da un altro punto
+    public double distance(Point other) {
+        return Math.sqrt(Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2));
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return Double.compare(point.x, x) == 0 &&
+                Double.compare(point.y, y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
 }
--- api/src/test/java/it/unicam/cs/pa/robotSwarm/PointTest.java
+++ api/src/test/java/it/unicam/cs/pa/robotSwarm/PointTest.java
@@ -1,13 +1,61 @@
 package it.unicam.cs.pa.robotSwarm;
 
 import org.junit.jupiter.api.Test;
+import it.unicam.cs.pa.robotSwarm.model.Point;
 
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PointTest {
     @Test
-    public void SampleTest()
-    {
+    public void testCreateAndGet() {
+        Point p = new Point(3.0, 4.0);
+        assertEquals(3.0, p.getX(), 0.001);
+        assertEquals(4.0, p.getY(), 0.001);
+    }
 
+    // Test per i metodi setX e setY
+    @Test
+    public void testSet() {
+        Point p = new Point(3.0, 4.0);
+        p.setX(5.0);
+        p.setY(6.0);
+        assertEquals(5.0, p.getX(), 0.001);
+        assertEquals(6.0, p.getY(), 0.001);
+    }
+
+    // Test per il metodo distance
+    @Test
+    public void testDistance() {
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(3, 4);
+        assertEquals(5.0, p1.distance(p2), 0.001);
+    }
+
+    // Test per il metodo toString
+    @Test
+    public void testToString() {
+        Point p = new Point(1, 2);
+        assertEquals("Point{x=1.0, y=2.0}", p.toString());
+    }
+
+    // Test per il metodo equals
+    @Test
+    public void testEquals() {
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(1, 2);
+        Point p3 = new Point(2, 1);
+
+        assertTrue(p1.equals(p2));
+        assertFalse(p1.equals(p3));
+    }
+
+    // Test per il metodo hashCode
+    @Test
+    public void testHashCode() {
+        Point p1 = new Point(1, 2);
+        Point p2 = new Point(1, 2);
+
+        assertEquals(p1.hashCode(), p2.hashCode());
     }
 
 }
