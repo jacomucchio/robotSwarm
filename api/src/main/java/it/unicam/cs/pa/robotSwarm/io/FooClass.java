86779a2f5b59b935ec74d4aa82888db0a10802d0
Jacopo Mucchietto <jacopo.mucchietto@studenti.unicam.it>
2024-01-12 14:51:07 +0100

    added Point Test

--- api/src/main/java/it/unicam/cs/pa/robotSwarm/io/FooClass.java
+++
@@ -1,4 +0,0 @@
-package it.unicam.cs.pa.robotSwarm.io;
-
-public class FooClass {
-}
---
+++ api/src/test/java/it/unicam/cs/pa/robotSwarm/PointTest.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.pa.robotSwarm;
+
+import org.junit.jupiter.api.Test;
+
+
+public class PointTest {
+    @Test
+    public void SampleTest()
+    {
+
+    }
+
+}
